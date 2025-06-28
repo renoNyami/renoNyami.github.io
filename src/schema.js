b4acd19c7b27b57cb2cbaafc26da1adb4f1f03ee
你的名字 <>
2025-06-29 00:46:14 +0800

    更新：添加元描述和SEO优化

---
+++ src/schema.js
@@ -0,0 +1,7 @@
+export const websiteSchema = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "CodeShare",
+  "url": "https://codeshare.example.com",
+  "description": "专业代码协作平台，支持实时多人编程和代码版本管理"
+};
